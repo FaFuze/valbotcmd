36333efe2f6f267f0edb0de7a1ed0f4bf8635abc
FaFuze <103941281+FaFuze@users.noreply.github.com>
2024-10-08 22:28:37 -0400

    Update script.js
    changed "not live" message

--- script.js
+++ script.js
@@ -8,7 +8,8 @@
   /* streamStartDateString will be a date string even if the channel is not currently live (the date will be the current
      date). This may be a Nightbot bug. This is why streamUptimeString is needed to check whether the channel is live */
   if (/\bnot live\b/i.test(streamUptimeString)) {
-    return `${playerName} is not live.`;
+    /* return `${playerName} is not live.`; */
+    return `Currently not live. Come back when I'm streaming to check my record!`;
   }
 
   const streamStartDate = new Date(streamStartDateString);
